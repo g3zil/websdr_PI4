a217234a46f09d927df027b10624b1c415037b75
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-08-10 15:28:04 +0100

    Create PI4_log.sh

---
+++ PI4_log.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+### This bash script with python parts to detect (sic - not decode, yet)and upload PI4 data from Margate 24 GHz SDR to a table in tutorial database on WD1
+### Basic functional prototype based on jt4 original by Gwyn G3ZIL Oct-Nov 2024
+### V1.0 for Raspberry Pi for G4BAO evaluation
+
+########################################################
+# Only user-set installation variables are in this block
+########################################################
+# set up receiver details to go into database table via python
+TX_CALL=PA3GCO          # PA3GCO dial frequency 24048.9320 to put carrier at 800 Hz and Tone0 at 682.8125 Hz
+#TX_CALL=GB3PKT         # for program testing of logic, not detection
+TX_GRID=JO21EU          # The tx details are needed here as we are detect only, and the SDR will be set to this beacon's frequency
+#TX_GRID=JO01MT         #
+BAND=3.0
+FREQUENCY=10368.9292    # dial for PA3GCO on 10 GHz PI4
+#FREQUENCY=24048.944      # dial for GB3PKT as above
+#RX_ID=TRIG01/G3ZIL      # The /G3ZIL is a suggested addition, use your callsign to show who is using it
+#RX_GRID=JO01qj
+RX_ID=SHBRG/G3ZIL      # This is SDR at http://sdr.shbrg.nl:8074/ The /G3ZIL is a suggested addition, use your callsign to show who is using it
+RX_GRID=JO21PR
+########################################################
+# set up base directory
+BASE_DIR=/users/gxg/desktop/PI4
+
+########################################################
+# main code
+#######################################################
+
+DECODE_CAPTURE_DATE=$(date -u  +%Y-%m-%dT%H:%M:00Z)       # easier to get time in required format this way
+WAV_FILE=$(ls -ltr ${BASE_DIR}/save| tail -n 1 | awk '{print $9}')              # get the PI4  wav file name to process, every minute
+WAV_FILE_TIME=$(ls -ltr ${BASE_DIR}/save| tail -n 1 | awk '{print substr($9, 8,4)}')
+LAST_MINUTE=$(date -u -v-1M +%Y-%m-%dT%H:%M:00Z)
+#echo "File time ${WAV_FILE_TIME}  last minute time ${LAST_MINUTE}"
+#if [[ ${WAV_FILE_TIME} = ${LAST_MINUTE} ]]
+#then
+    echo 'New data file - so detect PI4 and estimate noise'
+    echo "Detection program processing file "${WAV_FILE}
+    /usr/local/bin/sox ${BASE_DIR}/save/${WAV_FILE} ${BASE_DIR}/trimmed.wav trim 0 25
+    /usr/local/bin/sox ${BASE_DIR}/trimmed.wav -r 12000 ${BASE_DIR}/12000.wav              # resample to 12000 sps for PI4
+    /Users/gxg/miniconda3/bin/python3 ${BASE_DIR}/PI4_detect.py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/12000.wav  > ${BASE_DIR}/PI4_detect.log   # do the processing!
+    ${BASE_DIR}/PI4_sn_calc.sh ${WAV_FILE}                                       # script uses SOX to estimate noise
+    paste -d "," ${BASE_DIR}/noise.csv ${BASE_DIR}/PI4_detections.csv >${BASE_DIR}/temp.csv  # put both csv data sets on one line
+    awk -F"," -v OFS="," -v tx_call="${TX_CALL}" -v tx_call="${TX_CALL}" -v tx_grid="${TX_GRID}" -v band="${BAND}" -v frequency="${FREQUENCY}" \
+     -v rx_id="${RX_ID}" -v rx_grid="${RX_GRID}" ' {print $1,tx_call,tx_grid,band,frequency,rx_id,rx_grid,$2,$3,$5,$6,$7,$8,$9,$10,$11,$12,$13}' <${BASE_DIR}/temp.csv >${BASE_DIR}/data.csv
+    cat ${BASE_DIR}/data.csv
+    /Users/gxg/miniconda3/bin/python3 ${BASE_DIR}/PI4_upload.py ${BASE_DIR}/data.csv   # uploads to database with mode ident PI4 together with metadata
+#else
+ # echo "Stale file - possibly WSJT-X not running"
+#fi
+
+# Tidy up wav files, keep just last 10
+rm -v -f $(ls -1t ${BASE_DIR}/save/*.wav | tail -n +11)
+
+echo "Processing complete"
