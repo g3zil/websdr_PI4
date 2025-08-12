38015d7df09a01585cc67081387d32e1756e2d8e
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-08-12 11:25:41 +0100

    Update PI4_log.sh

--- PI4_log.sh
+++ PI4_log.sh
@@ -41,9 +41,10 @@ then
     echo "Detection program processing file "${WAV_FILE}
     sox ${BASE_DIR}/save/${WAV_FILE} ${BASE_DIR}/trimmed.wav trim 0 25    # The PI4 tones are in first 25 seconds
     sox ${BASE_DIR}/trimmed.wav -r 12000 ${BASE_DIR}/12000.wav            # resample to 12000 sps for PI4
-    /usr/bin/python3 ${BASE_DIR}/PI4_detect.py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/12000.wav  > ${BASE_DIR}/PI4_detect.log   # do the processing!
-
-    ${BASE_DIR}/sn_calc.sh ${WAV_FILE}                                # script uses SOX to estimate RMS noise
+    python3 .py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/12000.wav  > ${BASE_DIR}/PI4_detect.log   # do the processing!
+    sed -i 's/^M//g' file ${BASE_DIR}/PI4_detections.csv                  # Have to remove the control M carriage return at end of line
+    
+    ${BASE_DIR}/sn_calc.sh ${WAV_FILE}                                    # script uses SOX to estimate RMS noise
 
     paste -d "," ${BASE_DIR}/noise.csv ${BASE_DIR}/PI4_detections.csv >${BASE_DIR}/temp.csv  # put both csv data sets on one line
 
