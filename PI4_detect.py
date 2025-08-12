8c2e4939c5e5eebf2ed8a9b7fb1110a1034167c8
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-08-12 15:03:22 +0100

    Update PI4_detect.py

--- PI4_detect.py
+++ PI4_detect.py
@@ -178,9 +178,8 @@ plt.savefig(PLOT_FILE + '.png', dpi=300)
 #######################################################################################
 
 peaks = signal.find_peaks_cwt(correl_zoom, widths=np.arange(1,4))  # 2,4 is initial empirical selection
-print (peaks)
 peakind=remove_adjacent(peaks)                                     # in case single peak shown as two adj freqs
-
+print(peakind)
 with open(DETECTION_FILE, "w") as out_file:
   out_writer=csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
  
