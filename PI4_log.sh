9da7bcde570f9f9db48e223e0425d14f8d82eecf
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2026-02-27 10:21:50 +0000

    Update PI4_log.sh

--- PI4_log.sh
+++ PI4_log.sh
@@ -26,11 +26,15 @@ if [[ ${WAV_FILE_TIME} = ${LAST_MINUTE} ]]
 then
     echo 'New data file - so deBASE_DIR}/${WAV_FILE}tect PI4 and estimate noise'
     echo "Detection program processing file "${WAV_FILE}
-    sox ${BASE_DIR}/save/${WAV_FILE} ${BASE_DIR}/trimmed.wav trim 0 25       # The PI4 tones are in first 25 seconds
     FILE_NAME=${BASE_DIR}/${WAV_FILE}
     FILE_NAME=$(echo ${FILE_NAME%.*})
-    sox ${BASE_DIR}/trimmed.wav -r 12000 ${FILE_NAME}_12000.wav   # resample to 12000 sps for PI4
-    python3 ${BASE_DIR}/PI4_detect_new.py ${DECODE_CAPTURE_DATE} ${FILE_NAME}_12000.wav  > ${BASE_DIR}/PI4_detect.log   # do the processing!
+    sox ${BASE_DIR}/save/${WAV_FILE} -r 12000 ${BASE_DIR}/${FILE_NAME}_12000.wav   # resample to 12000 sps for PI4
+    echo "PI4 decode call in here"
+    sox ${BASE_DIR}/${FILE_NAME}_12000.wav ${BASE_DIR}/${FILE_NAME}_trimmed.wav trim 0 25       # The PI4 tones are in first 25 seconds
+    #FILE_NAME=${BASE_DIR}/${WAV_FILE}
+    #FILE_NAME=$(echo ${FILE_NAME%.*})
+    #sox ${BASE_DIR}/trimmed.wav -r 12000 ${FILE_NAME}_12000.wav   # resample to 12000 sps for PI4
+    python3 ${BASE_DIR}/PI4_detect_new.py ${DECODE_CAPTURE_DATE} ${BASE_DIR}/${FILE_NAME}_trimmed.wav  > ${BASE_DIR}/PI4_detect.log   # do the processing!
     sed -i 's/\r//g'  ${BASE_DIR}/PI4_detections.csv                         # Remove carriage return at end of line
     
     ${BASE_DIR}/sn_calc.sh ${WAV_FILE}                                       # script uses SOX to estimate RMS noise
